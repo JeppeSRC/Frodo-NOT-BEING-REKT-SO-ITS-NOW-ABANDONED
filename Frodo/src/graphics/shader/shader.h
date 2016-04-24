6a5fa0c7a9ab71b89ac2de925ef993992ecc6b15
JeppeSRC <jeppe97rc@gmail.com>
2016-04-24 18:31:43 +0200

    Renamed SetPSResourceView -> SetTexture

--- Frodo/src/graphics/shader/shader.h
+++ Frodo/src/graphics/shader/shader.h
@@ -41,7 +41,7 @@ public:
 
 	void SetVSConstantBuffer(unsigned int slot, void* data);
 	void SetPSConstantBuffer(unsigned int slot, void* data);
-	void SetPSResourceView(unsigned int slot, const Texture* tex);
+	void SetTexture(unsigned int slot, const Texture* tex);
 
 	inline const void* GetVSBufferPointer() const { return vByteCode->GetBufferPointer(); }
 	inline size_t GetVSBufferSize() const { return vByteCode->GetBufferSize(); }
