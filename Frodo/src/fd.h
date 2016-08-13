6be102cf339ef55155409a62c58eb0de6117171d
JeppeSRC <jeppe97rc@gmail.com>
2016-08-13 14:55:56 +0200

    Added NvOptimusEnablement

--- Frodo/src/fd.h
+++ Frodo/src/fd.h
@@ -6,3 +6,8 @@
 #else
 #define FDAPI __declspec(dllimport)
 #endif
+
+#ifdef NV_OPTIMUS_ENABLE
+extern "C" __declspec(dllexport) unsigned int NvOptimusEnablement = 0x1;
+#undef NV_OPTIMUS_ENABLE
+#endif
