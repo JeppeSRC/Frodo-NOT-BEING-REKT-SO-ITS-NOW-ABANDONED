fa6404af269047faa3c973a523ae0484fe2c39c8
JeppeSRC <jeppe97rc@gmail.com>
2016-09-17 11:07:52 +0200

    Mappable vertexbuffers

--- Frodo/src/graphics/buffer/vertexbuffer.cpp
+++ Frodo/src/graphics/buffer/vertexbuffer.cpp
@@ -1,15 +1,30 @@
 #include "vertexbuffer.h"
 #include <core/log.h>
 
-VertexBuffer::VertexBuffer(void* data, size_t size, unsigned int stride) : Buffer(), stride(stride) {
+
+VertexBuffer::VertexBuffer(unsigned int structSize, unsigned int num) {
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(D3D11_BUFFER_DESC));
+
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.ByteWidth = size = structSize * num;
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	bd.StructureByteStride = stride = structSize;
+	bd.Usage = D3D11_USAGE_DYNAMIC;
+
+	D3DContext::GetDevice()->CreateBuffer(&bd, 0, &buffer);
+	FD_ASSERT(buffer);
+}
+
+VertexBuffer::VertexBuffer(void* data, size_t size, unsigned int stride, bool dynamic) : Buffer(), stride(stride) {
 	D3D11_BUFFER_DESC bd;
 	ZeroMemory(&bd, sizeof(D3D11_BUFFER_DESC));
 
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	bd.ByteWidth = size;
-	bd.CPUAccessFlags = 0;
+	bd.CPUAccessFlags = dynamic ? D3D10_CPU_ACCESS_WRITE : 0;
 	bd.StructureByteStride = stride;
-	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.Usage = dynamic ? D3D11_USAGE_DYNAMIC : D3D11_USAGE_DEFAULT;
 
 	this->size = size;
 	this->stride = stride;
@@ -24,7 +39,27 @@ VertexBuffer::VertexBuffer(void* data, size_t size, unsigned int stride) : Buffe
 	FD_ASSERT(buffer);
 }
 
+void* VertexBuffer::Map(FD_MAP_FLAG flag) {
+	D3D11_MAPPED_SUBRESOURCE map;
+	ZeroMemory(&map, sizeof(D3D11_MAPPED_SUBRESOURCE));
+
+	D3DContext::GetDeviceContext()->Map((ID3D11Resource*)buffer, 0, (D3D11_MAP)flag, 0, &map);
+
+	FD_ASSERT(map.pData);
+
+	return map.pData;
+}
+
+void VertexBuffer::Unmap() {
+	D3DContext::GetDeviceContext()->Unmap((ID3D11Resource*)buffer, 0);
+}
+
 void VertexBuffer::Bind() {
 	unsigned int offset = 0;
 	D3DContext::GetDeviceContext()->IASetVertexBuffers(0, 1, &buffer, &stride, &offset);
+}
+
+void VertexBuffer::Bind(unsigned int slot) {
+	unsigned int offset = 0;
+	D3DContext::GetDeviceContext()->IASetVertexBuffers(slot, 1, &buffer, &stride, &offset);
 }
--- Frodo/src/graphics/buffer/vertexbuffer.h
+++ Frodo/src/graphics/buffer/vertexbuffer.h
@@ -1,12 +1,27 @@
 #pragma once
 #include "buffer.h"
 
+enum FD_MAP_FLAG {
+	FD_MAP_WRITE = D3D11_MAP_WRITE,
+	FD_MAP_WRITE_NO_OVERWRITE = D3D11_MAP_WRITE_NO_OVERWRITE,
+	FD_MAP_WRITE_DISCARD = D3D11_MAP_WRITE_DISCARD,
+	FD_MAP_READ = D3D11_MAP_READ,
+	FD_MAP_READ_WRITE = D3D11_MAP_READ_WRITE
+};
+
+
 class FDAPI VertexBuffer : public Buffer {
 private:
 	unsigned int stride;
+	unsigned int size;
 	
 public:
-	VertexBuffer(void* data, size_t size, unsigned int stride);
+	VertexBuffer(unsigned int structSize, unsigned int num);
+	VertexBuffer(void* data, size_t size, unsigned int stride, bool dynamic = false);
+
+	void* Map(FD_MAP_FLAG flag);
+	void Unmap();
 
 	void Bind() override;
+	void Bind(unsigned int slot = 0);
 };
