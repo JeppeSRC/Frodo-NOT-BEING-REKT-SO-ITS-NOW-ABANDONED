64494dcd2473598810ab8d059ee902c77a506535
JeppeSRC <jeppe97rc@gmail.com>
2016-11-18 22:56:10 +0100

    Fixed batch and font renderer and added color to font rendering

--- Frodo/src/graphics/shader/shaders/font_default_v.hlsl
+++ Frodo/src/graphics/shader/shaders/font_default_v.hlsl
@@ -3,6 +3,7 @@ R"(
 struct Out {
 	float4 position : SV_POSITION;
 	float2 texCoord : TEXCOORDS;
+	float4 color : COLOR;
 	float tid : TID;
 };
 
@@ -10,11 +11,12 @@ cbuffer view_data : register(b0) {
 	float4x4 projection;
 };
 
-Out vsMain(float2 position : POSITION, float2 texCoords : TEXCOORDS, float tid : TID) {
+Out vsMain(float2 position : POSITION, float2 texCoords : TEXCOORDS, float4 color : COLOR, float tid : TID) {
 	Out o;
 
 	o.position = mul(projection, float4(position, 0, 1));
 	o.texCoord = texCoords;
+	o.color = color;
 	o.tid = tid;
 
 	return o;
