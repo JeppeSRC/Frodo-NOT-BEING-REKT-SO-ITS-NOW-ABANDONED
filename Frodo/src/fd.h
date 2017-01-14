f25c743724ec33b453ef5730b744d6e09fc72ca0
JeppeSRC <jeppe97rc@gmail.com>
2017-01-14 11:12:55 +0100

    Added support for AMD PowerXpress

--- Frodo/src/fd.h
+++ Frodo/src/fd.h
@@ -7,7 +7,22 @@
 #define FDAPI __declspec(dllimport)
 #endif
 
+
+#define NV_OPTIMUS extern "C" __declspec(dllexport) unsigned int NvOptimusEnablement = 0x1;
+#define AMD_POWERXPRESS extern "C" __declspec(dllexport) unsigned int AmdPowerXpressRequestHighPerformance = 0x1;
+
 #ifdef NV_OPTIMUS_ENABLE
-extern "C" __declspec(dllexport) unsigned int NvOptimusEnablement = 0x1;
+NV_OPTIMUS
 #undef NV_OPTIMUS_ENABLE
+#endif
+
+#ifdef AMD_POWERXPRESS_ENABLE
+AMD_POWERXPRESS
+#undef AMD_POWERXPRESS_ENABLE
+#endif
+
+#ifdef GPU_REQUEST_HIGH_PERFORMANCE
+NV_OPTIMUS
+AMD_POWERXPRESS
+#undef GPU_REQUEST_HIGH_PERFORMANCE
 #endif
