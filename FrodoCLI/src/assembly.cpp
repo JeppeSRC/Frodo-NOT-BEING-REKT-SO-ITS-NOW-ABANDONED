feb105812d0f2b6d8246772886b48433e4c0fb00
JeppeSRC <jeppe97rc@gmail.com>
2017-03-14 17:39:07 +0100

    CLI binding

---
+++ FrodoCLI/src/assembly.cpp
@@ -0,0 +1,9 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[AssemblyTitleAttribute(L"FrodoCLI")];
+[AssemblyDescriptionAttribute(L"Contains CLI bindings for Frodo.dll")];
+
