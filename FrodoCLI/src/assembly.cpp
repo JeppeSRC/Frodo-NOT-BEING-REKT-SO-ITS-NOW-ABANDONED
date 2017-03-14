8707e274f083ec9bc720dd257d6428f5bfd0183c
JeppeSRC <jeppe97rc@gmail.com>
2017-03-14 20:46:20 +0100

    Fixed errors

--- FrodoCLI/src/assembly.cpp
+++ FrodoCLI/src/assembly.cpp
@@ -4,6 +4,6 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-[AssemblyTitleAttribute(L"FrodoCLI")];
-[AssemblyDescriptionAttribute(L"Contains CLI bindings for Frodo.dll")];
+[assembly:AssemblyTitleAttribute(L"FrodoCLI")];
+[assembly:AssemblyDescriptionAttribute(L"Contains CLI bindings for Frodo.dll")];
 
