2792572ad85c142fabe9f8247baf439ed3394e17
JeppeSRC <jeppe97rc@gmail.com>
2017-03-18 11:52:41 +0100

    Added sandbox C# project

---
+++ SandboxCS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FDCLI;
+
+namespace SandboxCS {
+    class Program {
+        static void Main(string[] args) {
+
+            /* Asset asset = new Asset();
+
+             asset.name = "Asset";
+             asset.folder = "FOlder";
+             asset.type = FD_ASSET_TYPE.FD_ASSET_TYPE_RAW;
+             asset.packageName = "package name not set";
+
+             asset.SetData("kebab.txt");
+
+             Package package = new Package();
+
+             package.name = "Package";
+
+             package.AddAsset(ref asset);
+
+             AssetManager.ExportPackage("Test.pkg", ref package);*/
+
+            AssetManager.LoadPackage("Test.pkg");
+
+            
+        }
+
+    }
+}
